83f924e83e7173cc894deaa42131ea34ee2ce008
Cilibock <Cilibock@mail.ru>
2025-02-02 21:26:20 +0300

    1st commit

---
+++ player.py
@@ -0,0 +1,8 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super.__init__(group)
+
+        self.image = pygame.Surface()
