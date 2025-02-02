016b09b29fffe6bbc61b89913e141e1c3f1286bb
imdvks <Midavikus@yandex.ru>
2025-02-03 02:21:09 +0300

    overlay update

--- code/level.py
+++ code/level.py
@@ -1,13 +1,14 @@
 import pygame
 from player import Player
 from settings import *
-
+from overlay import Overlay
 
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
         self.all_sprites = pygame.sprite.Group()
         self.setup()
+        self.overlay = Overlay(self.player)
 
     def setup(self):
         self.player = Player((640, 360), self.all_sprites)
@@ -16,3 +17,5 @@ class Level:
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(time)
+
+        self.overlay.display()
