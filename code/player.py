a2e81d0ce1a3c6ed9991dd5c572ad5b9af9d3025
imdvks <Midavikus@yandex.ru>
2025-02-03 00:17:25 +0300

    support commit

--- code/player.py
+++ code/player.py
@@ -1,6 +1,6 @@
 import pygame
 from settings import *
-
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
@@ -14,6 +14,15 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
     def input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
